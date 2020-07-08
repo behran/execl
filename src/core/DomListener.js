e92d7da4da6d82a80640f0aabd5d18aa62cffb29
behran <agolovev@gismeteo.ru>
2020-07-08 20:23:06 +0300

    lesson 35 finish

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import {capitalize} from '@core/utils'
+
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -8,10 +10,25 @@ export class DomListener {
   }
 
   initDOMListeners() {
-
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        const name = this.name || ''
+        // eslint-disable-next-line max-len
+        throw new Error(`Method ${method} is not implemented in ${name} Component`)
+      }
+      // this.$root - экзмепляр класса Dom
+      // Так как это JS и тереяется контекст у метода то привзяываем через this
+      this.$root.on(listener, this[method].bind(this))
+    })
   }
 
   removeDOMListeners() {
 
   }
+}
+
+// input-> onInput
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
